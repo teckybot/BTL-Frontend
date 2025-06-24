fdcb60cba5366bac52e5907a1610986487c9e94b
B.K.S.S.S MALLIK <sssmallik143@gmail.com>
2025-06-24 22:42:31 +0530

    updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
     // headers: {
     //     'Content-Type': 'application/json'
     // },
-    timeout: 10000, // 10 second timeout
+    timeout: 60000, // 10 second timeout
 
 });
 
